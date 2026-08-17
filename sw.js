ba3792259d3ec70f878bd501d032e002b3a7dd2a
Suncica1212 <suncica@bluewin.ch>
2026-08-17 18:26:05 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tram-trainer-final-github-v3';
+const CACHE='tram-trainer-v5-knotenpunkte';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./tramnetz-2026.png','./tramnetz-quiz-ohne-ortschaften.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
